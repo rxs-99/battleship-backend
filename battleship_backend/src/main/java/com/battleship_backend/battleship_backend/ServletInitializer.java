65d243d2c5bbe341863791247e84bbdf254adbb9
Rabinson Shrestha <rabinshrestha4100@gmail.com>
2021-01-11 09:37:57 -0600

    initialized Spring Boot project

---
+++ battleship_backend/src/main/java/com/battleship_backend/battleship_backend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.battleship_backend.battleship_backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BattleshipBackendApplication.class);
+	}
+
+}
---
+++ battleship_backend/src/test/java/com/battleship_backend/battleship_backend/BattleshipBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battleship_backend.battleship_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
